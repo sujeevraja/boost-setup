1c8e9e045c35429fa84011bbba72b1a8c3e002e3
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-01-20 09:29:14 -0700

    add console logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include "boost/log/trivial.hpp"
+#include "boost/log/utility/setup.hpp"
 #include <iostream>
 
-int main(int argc, char * argv[])
+namespace logging = boost::log;
+// namespace sinks = boost::log::sinks;
+// namespace src = boost::log::sources;
+// namespace expr = boost::log::expressions;
+// namespace attrs = boost::log::attributes;
+namespace keywords = boost::log::keywords;
+
+void configureLogging();
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!\n";
+    BOOST_LOG_TRIVIAL(info) << "Hello, world!\n";
     return 0;
+}
+
+void configureLogging()
+{
+    logging::add_console_log(
+        std::cout,
+        keywords::auto_flush = true);
+
+    logging::add_common_attributes();
 }
