869a6bd9ef8b68acf0da881e4a7ca911709a3407
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-01-20 09:31:00 -0700

    add format to console logging

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,17 @@ void configureLogging();
 
 int main(int argc, char *argv[])
 {
+    configureLogging();
     BOOST_LOG_TRIVIAL(info) << "Hello, world!\n";
     return 0;
 }
 
 void configureLogging()
 {
+    static const auto format = std::string{"[%TimeStamp%] [%Severity%]:  %Message%"};
     logging::add_console_log(
         std::cout,
+        keywords::format = format,
         keywords::auto_flush = true);
 
     logging::add_common_attributes();
