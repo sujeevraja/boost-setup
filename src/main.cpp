0610d83a7300f5db5cc114cb2352942bf8ee6358
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-01-20 13:20:51 -0700

    add comments, cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "boost/log/expressions/formatters/date_time.hpp"
 #include "boost/log/support/date_time.hpp"
 
+// These 2 headers are needed for the BOOST_LOG_ATTRIBUTE_KEYWORD macro.
 #include "boost/log/expressions/keyword_fwd.hpp"
 #include "boost/log/expressions/keyword.hpp"
 
@@ -15,43 +16,24 @@
 #include <iostream>
 
 namespace logging = boost::log;
-// namespace sinks = boost::log::sinks;
-namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
-namespace attrs = boost::log::attributes;
-namespace keywords = boost::log::keywords;
 
 void configureLogging();
 void coloring_formatter(logging::record_view const &rec,
                         logging::formatting_ostream &strm);
 
-enum severity_level
-{
-    debug,
-    info,
-    warning,
-    error
-};
-
-BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", attrs::local_clock::value_type);
+// This macro use is needed to access the timestamp using rec[timestamp] in
+// the coloring formatter function.
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp",
+                            logging::attributes::local_clock::value_type);
 
 int main(int argc, char *argv[])
 {
-    // ----- basic logging
     configureLogging();
     BOOST_LOG_TRIVIAL(debug) << "Hello, world!";
     BOOST_LOG_TRIVIAL(info) << "Hello, world!";
     BOOST_LOG_TRIVIAL(warning) << "Hello, world!";
     BOOST_LOG_TRIVIAL(error) << "Hello, world!";
     BOOST_LOG_TRIVIAL(fatal) << "Hello, world!";
-
-    // ---- simple custom logger.
-    // src::logger lg;
-    // BOOST_LOG(lg) << "Hello, world!\n";
-
-    // ----- Creates a severity logger with custom severity levels.
-    // src::severity_logger<severity_level> lg;
-    // BOOST_LOG_SEV(lg, debug) << "Hello, World\n";
 
     return 0;
 }
@@ -61,7 +43,7 @@ void configureLogging()
     logging::add_console_log(
         std::cout,
         // keywords::format = format,
-        keywords::auto_flush = true)
+        logging::keywords::auto_flush = true)
         ->set_formatter(&coloring_formatter);
 
     static const auto format = std::string{"[%TimeStamp%] [%Severity%]:  %Message%"};
@@ -100,7 +82,8 @@ void coloring_formatter(
 
     // Format the message here...
     strm << "[" << rec[timestamp] << "]["
-         << std::setw(8) << severity << "] " << rec[expr::smessage];
+         << std::setw(8) << severity << "] "
+         << rec[logging::expressions::smessage];
 
     if (severity)
     {
